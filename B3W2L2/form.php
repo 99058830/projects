3c55867670d8b355cc7154a08f5e87bd6672dff5
JOZO <99058830@mydavinci.nl>
2022-06-14 10:14:37 +0200

    GET to POST

--- B3W2L2/form.php
+++ B3W2L2/form.php
@@ -2,8 +2,8 @@
 <body>
 
 <h1>De ingevulde gegevens zijn:</h1>
-Naam: <?php echo $_GET["name"]; ?><br>
-E-mail: <?php echo $_GET["email"]; ?>
+Naam: <?php echo $_POST["name"]; ?><br>
+E-mail: <?php echo  $_POST["email"]; ?>
 
 </body>
 </html>
